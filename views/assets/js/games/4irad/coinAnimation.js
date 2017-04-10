8c8cd21f26a7b740cc3d185e08aad7603ea4bcf2
bos79 <bjornsodergren01@gmail.com>
2017-04-10 19:43:51 +0200

    SpelPlanOchAddering av mynt

--- views/assets/js/games/4irad/coinAnimation.js
+++ views/assets/js/games/4irad/coinAnimation.js
@@ -1,10 +1,118 @@
 $(document).ready(function(){
-    $(".white-and-round").hover(function(){
+    createBord();
+    resizer();
+    $(window).resize(resizer);
+    columnClick();
+
+
+    // create the board
+    function createBord(){
+        for(let i = 0; i <= 5; i++){
+            for(let j = 0; j <= 6; j++){
+                  $('#Bord').append(' <div class="col row-'+i+'-col-'+j+'"><div class="white-and-round"></div></div>');
+            }
+        }
+    }
+    let i =5;  
+    let counter =0;
+    let player1 = new Player(10,"Björn","green",24,1);
+    let player2 = new Player(50,"Nisse","yellow",24,2);
+
+    function addCoin(column){
+         counter++;
+        console.log(counter%2);
+        let value = counter%2;
+        let coinColor;
+        let id;
+
+        if(value===0)
+        {
+             id = player1.id;
+             coinColor = player1.colore;
+        }
+        else if(value===1){
+             id = player2.id;
+            coinColor = player2.colore;
+        }
+      console.log(coinColor);
+       
+            if(i ===5 && !$(".row-"+i+"-col-"+column).find(".white-and-round").attr("id"))
+            {
+                
+                val=i+column+id;
+               $(".row-"+i+"-col-"+column).find('.white-and-round').css('background-color',coinColor).attr("id",val);
+               
+            }
+            else if(i===5){
+                 i--;   
+            }
+            
+             if(i===4 && !$(".row-"+i+"-col-"+column).find(".white-and-round").attr("id")){
+                
+                 val=i+column+id;
+               $(".row-"+i+"-col-"+column).find('.white-and-round').css('background-color',coinColor).attr("id",val);
+               i=5;
+            }
+            else if(i===4){
+                 i--;   
+            }
+            
+             if(i===3 && !$(".row-"+i+"-col-"+column).find(".white-and-round").attr("id")){
+                
+                 val=i+column+id;
+               $(".row-"+i+"-col-"+column).find('.white-and-round').css('background-color',coinColor).attr("id",val);
+               i=5;
+            }
+            else if(i===3){
+                 i--;   
+            }
+             if(i===2 && !$(".row-"+i+"-col-"+column).find(".white-and-round").attr("id")){
+                
+                 val=i+column+id;
+               $(".row-"+i+"-col-"+column).find('.white-and-round').css('background-color',coinColor).attr("id",val);
+               i=5;
+            }
+            else if(i===2){
+                 i--;   
+            }
+          
+             if(i===1 && !$(".row-"+i+"-col-"+column).find(".white-and-round").attr("id")){
+                
+                 val=i+column+id;
+               $(".row-"+i+"-col-"+column).find('.white-and-round').css('background-color',coinColor).attr("id",val);
+               i=5;
+            }
+            else if(i===1){
+                 i--;   
+            }
+              if(i===0 && !$(".row-"+i+"-col-"+column).find(".white-and-round").attr("id")){
+                
+                 val=i+column+id;
+               $(".row-"+i+"-col-"+column).find('.white-and-round').css('background-color',coinColor).attr("id",val);
+               i=5;
+            }
+           
+            
+        
+
+    }
+
+    // click on a column (really on a slot)
+    function columnClick(){
+        $('#Bord .col').click(function(){
+            console.log($(this).attr('class'));
+            let column = $(this).attr('class').split('-col-')[1];
+            console.log("You clicked column",column);
+            addCoin(column);
+        });
+    }
+  
+    /*$(".white-and-round").hover(function(){
         $(this).css("background-color", "yellow");},
           function(){
         $(this).css("background-color", "white");
     
-    });
+    });*/
     $(".col-md-1").click(function(){
     	console.log(
     		$(this).find('.white-and-round').css('background-color')
@@ -18,9 +126,12 @@ $(document).ready(function(){
     	let w = $('.white-and-round').width();
     	$('.white-and-round').height(w);
     }
-   	$(window).resize(resizer); // run resizer each time the window changes size
-   	resizer(); // and run it once initially too
+   	 // run resizer each time the window changes size
+   	 // and run it once initially too
 
-
+    $('#field').on('click', function (e) {
+        console.log($('#field').width(), $('#field').height());
+        alert(e.pageX + ' , ' + e.pageY);
+    });
 
 });
--- views/assets/js/games/4irad/playerClass.js
+++ views/assets/js/games/4irad/playerClass.js
@@ -1,10 +1,19 @@
+$(document).ready(function(){
+	$('#field').on('click', function (event) {
+		console.log($('#filed').width(), $('#filed').height());
+	});
+});
+
 class Player{
 
-	constructor(score,name){
+	constructor(score,name,colore, coins,id){
+		this.id =id;
 		this.name = name;
 		this.score= score;
-
+		this.colore= colore;
+		this.coins= coins;
 	}
+	
 }
 class Computer extends Player{
 
@@ -30,3 +39,4 @@ class Hard extends Computer{
 
 	}
 }
+
