53216c93906425961d473e31867e53fc4f279212
BharathiHarsha <bharthi.harsha@gmail.com>
2017-04-19 15:02:31 +0200

    Made changes in Static script.

---
+++ views/assets/js/games/4irad/login.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    addClickHandlersForButtons();
+});
+
+function addClickHandlersForButtons(){
+    
+    $('#login').click(function(){
+        let name = $('#inputUsername').val();
+        let mail = $('#inputEmail').val();
+        let password = $('#inputPassword').val();
+        
+        if(!isNaN(name)){
+              alert("Name is not valid");
+
+            }
+        if(password.length<8 || !isNaN(password)){
+
+            alert("Password is not valid");
+            }
+        if(!isNaN(mail)){
+
+            alert("Enter valid email-Id");
+           }     
+        
+    });
+
+    $('#logout').click(function() {
+        window.location.hash = '#home';
+    });
+
+}
