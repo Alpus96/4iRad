497aa837929e1d0daaa00ee6c7814d6833904158
Alexander Ljungberg Perme <alex.perme@gmail.com>
2017-04-21 11:48:42 +0200

    fixed unexpected { error

--- views/assets/js/games/4irad/coinAnimation.js
+++ views/assets/js/games/4irad/coinAnimation.js
@@ -274,71 +274,71 @@ $("#mydiv").width(width);*/
         alert(e.pageX + ' , ' + e.pageY);
     });
 
+
+
+    checkForWinner(players) {
+        for (player of players) {
+            let inARow;
+            let last;
+            for (column of this.gameBoard) {
+                inARow = 1;
+                last = 0;
+                for (let i = 0; i < column.length; i++) {
+                    if (column[i].owner === player.id) {
+                        if (last === i - 1) {
+                            inARow++;
+                        } else {
+                            inARow = 1;
+                        }
+                        last = i;
+                    }
+                }
+                inARowCheck(player, inARow);
+            }
+
+            for (let i = 0; i < this.gameBoard.length; i++) {
+                last = 0;
+                for (let j = 0; i < this.gameBoard[j].length; j++) {
+                    if(this.gameBoard[j][i].owner === player.id) {
+                        if (last = j -1) {
+                            inARow++;
+                        } else {
+                            inARow = 1;
+                        }
+                        last = j;
+                    }
+                }
+                inARowCheck(player, inARow);
+            }
+
+            for (let i = 0; i < this.gameBoard.length; i++) {
+                for (let j = 0; i < this.gameBoard[i].length; j++) {
+                    if (this.gameBoard[i][j].owner === player.id) {
+                        let k = 0;
+                        let inARow = 1;
+                        while (this.gameBoard[i-k][j-k].owner === player.id) {
+                            inARow++;
+                        }
+                        while (this.gameBoard[i+k][j+k].owner === player.id) {
+                            inARow++;
+                        }
+                        inARowCheck(player, inARow);
+                        while (this.gameBoard[i+k][j-k].owner === player.id) {
+                            inARow++;
+                        }
+                        while (this.gameBoard[i-k][j+k].owner === player.id) {
+                            inARow++;
+                        }
+                        inARowCheck(player, inARow);
+                    }
+                }
+            }
+        }
+    }
+
+    inARowCheck (player, inARow) {
+        if (inARow > 3) {
+            player.wins();
+        }
+    }
 });
-
-
-checkForWinner(players) {
-    for (player of players) {
-        let inARow;
-        let last;
-        for (column of this.gameBoard) {
-            inARow = 1;
-            last = 0;
-            for (let i = 0; i < column.length; i++) {
-                if (column[i].owner === player.id) {
-                    if (last === i - 1) {
-                        inARow++;
-                    } else {
-                        inARow = 1;
-                    }
-                    last = i;
-                }
-            }
-            inARowCheck(player, inARow);
-        }
-
-        for (let i = 0; i < this.gameBoard.length; i++) {
-            last = 0;
-            for (let j = 0; i < this.gameBoard[j].length; j++) {
-                if(this.gameBoard[j][i].owner === player.id) {
-                    if (last = j -1) {
-                        inARow++;
-                    } else {
-                        inARow = 1;
-                    }
-                    last = j;
-                }
-            }
-            inARowCheck(player, inARow);
-        }
-
-        for (let i = 0; i < this.gameBoard.length; i++) {
-            for (let j = 0; i < this.gameBoard[i].length; j++) {
-                if (this.gameBoard[i][j].owner === player.id) {
-                    let k = 0;
-                    let inARow = 1;
-                    while (this.gameBoard[i-k][j-k].owner === player.id) {
-                        inARow++;
-                    }
-                    while (this.gameBoard[i+k][j+k].owner === player.id) {
-                        inARow++;
-                    }
-                    while (this.gameBoard[i+k][j-k].owner === player.id) {
-                        inARow++;
-                    }
-                    inARowCheck(player, inARow);
-                    while (this.gameBoard[i-k][j+k].owner === player.id) {
-                        inARow++;
-                    }
-                    inARowCheck(player, inARow);
-                }
-            }
-        }
-    }
-}
-
-inARowCheck (player, inARow) {
-    if (inARow > 3) {
-        player.wins();
-    }
-}
