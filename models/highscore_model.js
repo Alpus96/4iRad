730725bf991d24e0ebb0dfd62ba02b93376c8efc
Alexander Ljungberg Perme <alex.perme@gmail.com>
2017-04-22 15:50:44 +0200

    highscore model

--- models/highscore_model.js
+++ models/highscore_model.js
@@ -1,19 +1,73 @@
 const DB = require('./dbConn');
-//const MySQL = require('mysql');
 
 class Highscore{
-    constructor () {
 
+    create (data) {
+        DB.connection.query('INSERT INTO highscore SET ?', data, (error, result) => {
+            if (!error) {
+                return true;
+            } else {
+                throw new Error(error);
+            }
+        });
     }
 
-    getTopTen(callback) {
-        DB.connection.query('SELECT * FROM highscore', function (error, result) {
+    read (callback) {
+        DB.connection.query('SELECT * FROM highscore ORDER BY score ASC', function (error, result) {
             if (!error) {
-                console.log(result);
+                callback(null, result);
             } else {
                 callback(error, null);
             }
         });
     }
 
+    update (data) {
+        this.read((error, result) => {
+            if (!error) {
+                if (result.legth < 10) {
+                    try {
+                        this.create(data);
+                    } catch (error) {
+                        return false;
+                    }
+                } else {
+                    let qualified = false;
+                    for (let highscore of result) {
+                        if (data.score < highscore.score) {
+                            qualified = true;
+                            break;
+                        }
+                    }
+                    if (qualified) {
+                        try {
+                            this.delete(result[9].id);
+                            this.create(data);
+                            return true;
+                        } catch (error) {
+                            console.error(error);
+                            return false;
+                        }
+                    } else {
+                        return false;
+                    }
+                }
+            } else {
+                throw new Error(error);
+            }
+        });
+    }
+
+    delete (id) {
+        DB.connection.query('DELETE FROM highscore WHERE id = ?', id, function (error) {
+            if (!error) {
+                return true;
+            } else {
+                throw new Error(error);
+            }
+        });
+    }
+
 }
+
+module.exports = new Highscore();
