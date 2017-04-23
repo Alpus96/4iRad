53c127b8a408efea8224270a257437e8e6b415ca
Alexander Ljungberg Perme <alex.perme@gmail.com>
2017-04-23 10:35:41 +0200

    Added a database model for users

--- models/userModell.js
+++ models/userModell.js
@@ -1,18 +1,115 @@
-const MySQL = require('mysql');
-const dbConn = require('./dbConn');
+const DB = require('./dbConn');
 
-class users {
-	constructor () {
+//	NOTE: 'Bcryptjs' is ~30% slower than 'bcrypt'. (Since it
+//	uses pure javascript and not c++ bcrypt binding.)
+const bCrypt = require('bcryptjs');
 
+class User {
+	ceate (data) {
+		try {
+			const existing = this.read(data.username);
+		} catch (error) {
+			console.error(error);
+			return false;
+		}
+
+		if (!existing) {
+			//	NOTE: 12 rounds ≈ 2 hashes per second with 'bcryptjs'. (~3 with 'bcrypt'.)
+			bCrypt.genSalt(12, (error, salt) => {
+				if (!error) {
+					bCrypt.hash(data.password, salt, (error, passwordHash) => {
+						data.password = passwordHash;
+						DB.connection.query('INSERT INTO users SET ?', data, (error, result) => {
+							if (!error) {
+								return true;
+							} else {
+								throw new Error(error);
+							}
+						});
+					});
+				} else {
+					throw new Error(error);
+				}
+			});
+		} else {
+			return false;
+		}
 	}
 
-	getUser (username) {
-		dbConn.connection.query('query string', function (error, response) {
+	read (username) {
+		DB.connection.query('SELECT * FROM users WHERE username = ? LIMIT 1', username, (error, result) => {
 			if (!error) {
-				
+				if (result.length > 0) {
+					return result;
+				} else {
+					return false;
+				}
 			} else {
-				
+				throw new Error(error);
 			}
 		});
 	}
+
+	validate (cridentials) {
+		let user;
+		try {
+			user = this.read(cridentials.username);
+		} catch (error) {
+			console.error(error);
+			return false;
+		}
+
+		bCrypt.compare(cridentials.password, user.password, (error, result) => {
+			if (!error) {
+				return result;
+			} else {
+				throw new Error(error);
+			}
+		});
+	}
+
+	update (data) {
+		let user;
+		try {
+			user = this.read(data.username);
+		} catch (error) {
+			console.error(error);
+			return false;
+		}
+
+		if (user) {
+			DB.connection.query('UPDATE users SET ? WHERE id = ?', [data, user.id], (error, result) => {
+				if (!error) {
+					return result;
+				} else {
+					throw new Error(error);
+				}
+			});
+		} else {
+			throw new Error('User does not exist. Invalid update request.')
+		}
+	}
+
+	delete (cridentials) {
+		let valid;
+		try {
+			valid = this.validate(cridentials);
+		} catch (error) {
+			console.error(error);
+			return false;
+		}
+
+		if (valid) {
+			DB.connection.query('DELETE FROM users WHERE username = ?', cridentials.username, (error, result) => {
+				if (!error) {
+					return result;
+				} else {
+					throw new Error(error);
+				}
+			});
+		} else {
+			return false;
+		}
+	}
+
 }
