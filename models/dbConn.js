95211865deee31c9592c5b5961024f5740245100
Alexander Ljungberg Perme <alex.perme@gmail.com>
2017-04-26 12:21:01 +0200

    fixed error

--- models/dbConn.js
+++ models/dbConn.js
@@ -17,15 +17,15 @@ class DBConn {
         // Recreate the connection, since the old one cannot be reused.
         this.connection = MySQL.createConnection(dbConf);
 
+        const db = this;
         // The server is either down or restarting (takes a while sometimes).
         this.connection.connect(function(err) {
             if(err) {
                 console.log('Database connection error:', err);
-                setTimeout(connect, 2000); // We introduce a delay before attempting to reconnect,
+                setTimeout(db.connect, 2000); // We introduce a delay before attempting to reconnect,
             } // to avoid a hot loop, and to allow our node script to process asynchronous requests in the meantime.
         });
 
-        const db = this;
         // If you're also serving http, display a 503 error.
         this.connection.on('error', function(err) {
             console.log('Database error:', err);
